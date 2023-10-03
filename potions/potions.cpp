30341385ef30225961bf05f05135101bbe0f3f57
Lucas Otanez <lotanez@usc.edu>
2023-10-02 17:53:31 -0700

    tweak potions indices are wrong

--- potions/potions.cpp
+++ potions/potions.cpp
@@ -14,7 +14,7 @@ bool ratio(int a1, int b1, int a2, int b2);
 int main() {
     vector<int> costs = {3, 2, 1, 5, 8};
     vector<int> powderA = {1, 3, 2, 3, 4};
-    vector<int> powderB = {3, 6, 1, 2, 5};
+    vector<int> powderB = {2, 6, 1, 2, 5};
     int Ma = 1;
     int Mb = 3;
 
@@ -35,15 +35,19 @@ int solve(vector<int> a, vector<int>b, vector<int> c, int Ma, int Mb) {
 
     vector<vector<vector<int>>> MP(n+1, (vector<vector<int>> (aTotal+1, vector<int> (bTotal+1, 100000))));
 
-    MP[0][0][0] = 0;
+    for (int i = 0; i < n; i++) {
+        MP[i][0][0] = 0;
+    }
 
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= aTotal; j++) {
             for (int k = 1; k <= bTotal; k++) {
                 MP[i][j][k] = MP[i-1][j][k];
-                if ((j-a[i] >= 0) && (k-b[i] >= 0)) {
-                    if ((MP[i-1][j-a[i]][k-b[i]] + c[i]) < MP[i][j][k]) {
-                        MP[i][j][k] = (MP[i-1][j-a[i]][k-b[i]] + c[i]);
+                // FIX:
+                // These indices are all fucked
+                if ((j-a[i-1] >= 0) && (k-b[i-1] >= 0)) {
+                    if ((MP[i-1][j-a[i-1]][k-b[i-1]] + c[i-1]) < MP[i][j][k]) {
+                        MP[i][j][k] = (MP[i-1][j-a[i-1]][k-b[i-1]] + c[i-1]);
                     }
                 }
             }
